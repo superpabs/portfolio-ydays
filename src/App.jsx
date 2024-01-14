a0c003cdd38957889ff3ae62ff3df96f702d3f2b
Precious <precious.soriano@ynov.com>
2024-01-14 23:08:34 +0100

    navbar and sidebar added

---
+++ src/App.jsx
@@ -0,0 +1,50 @@
+import "./app.scss";
+import Navbar from "./components/navbar/Navbar";
+
+const App = () => {
+  return <div>
+
+    <section id="Homepage">
+      
+      <Navbar/>
+    
+    </section>
+
+    <section id="Aboutt Me">
+      Parallax
+    </section>
+    <section>
+      About Me
+    </section>
+
+    <section id="Experiences">
+      Parllax
+    </section>
+    <section>
+      Experiences
+    </section>
+
+    <section id="Portfolio">
+      Parallax
+    </section>
+    <section>
+      Portfolio 1
+    </section>
+    <section>
+      Portfolio 2
+    </section>
+    <section>
+      Portfolio 3
+    </section>
+
+    <section id="Contact">
+      Contact
+    </section>
+
+    <section>
+      Footer
+    </section>
+  </div>;
+};
+
+export default App;
